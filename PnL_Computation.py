e53d864f33066c6dbd0d3ff34de86d56d087ed97
niroojane <116459392+niroojane@users.noreply.github.com>
2025-08-21 13:45:57 +0200

    Add files via upload

--- PnL_Computation.py
+++ PnL_Computation.py
@@ -52,7 +52,7 @@ class PnL:
         price=pd.concat([trade_history.reset_index(),price['Pair Price']],axis=1)
         price['Price in USDT']=np.where(price['Market'].str[-4:]=='USDT',price['Price'],price['Price'].astype(float)*price['Pair Price'].astype(float))
         price['Total in USDT']=(price['Price in USDT'].astype(float))*(price['Amount'].astype(float))
-        price['Pair Quantity']=price['Price in USDT']/price['Total in USDT']
+        price['Pair Quantity']=price['Total in USDT']/price['Price in USDT']
         
         return price
     
