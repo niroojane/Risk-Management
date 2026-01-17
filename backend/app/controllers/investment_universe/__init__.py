bb8e7576c388fc363495260e016071cb4d0b75bf
bourdinalix <alix.bourdin@epitech.eu>
2026-01-17 18:29:45 +0100

    remove prices controller

--- backend/app/controllers/investment_universe/__init__.py
+++ backend/app/controllers/investment_universe/__init__.py
@@ -1,6 +1,5 @@
 """Investment Universe Controllers"""
 from .market_cap_controller import MarketCapController
-from .prices_controller import PricesController
 from .positions_controller import PositionsController
 
 __all__ = ["MarketCapController", "PositionsController"]
